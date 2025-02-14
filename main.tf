95cff310c87d0b70bbac1c7bfdfbb60631a7b715
Aniketvaidhya <54594186+Aniketvaidhya@users.noreply.github.com>
2025-02-14 15:56:06 +0530

    Added tags

--- main.tf
+++ main.tf
@@ -9,6 +9,8 @@ resource "aws_cloudwatch_log_metric_filter" "metric_filter" {
   }
   name    = var.metric_filter_name != "" ? var.metric_filter_name : var.alarm_name
   pattern = var.pattern
+
+  tags = var.tags
 }
 
 resource "aws_cloudwatch_metric_alarm" "metric_alarm" {
@@ -23,9 +25,5 @@ resource "aws_cloudwatch_metric_alarm" "metric_alarm" {
   statistic           = var.statistic
   alarm_actions       = var.alarm_action_arns
   treat_missing_data  = var.treat_missing_data
-  tags = {
-    notification_type = var.notification_type,
-    severity          = var.severity,
-    Name              = var.alarm_name
-  }
+  tags = tags
 }
