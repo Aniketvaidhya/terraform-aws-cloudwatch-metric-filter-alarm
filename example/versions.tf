e586bb5b01e6bdf813169f5c68e469ad544c1ae1
Aniket Vaidhya <aniket.vaidhya@volkswagen.co.in>
2025-02-14 17:45:24 +0530

    Updated the version

--- example/versions.tf
+++ example/versions.tf
@@ -1,10 +1,10 @@
 terraform {
-  required_version = "~> 1.0.11"
+  required_version = "~> 1.3.10"
 
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 4.53.0"
+      version = "~> 5.50.0"
     }
   }
 }
